8ed73584c7420b534c1d7237b1a63c400b51ded8
Yan Batista <ybatistasouza@hotmail.com>
2020-09-07 17:44:42 -0300

    manifest

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(static(__dirname + '/build'));
 
 if(process.env.NODE_ENV === "production") {
-    app.get('/', (req,res) => {
+    app.get('*', (req,res) => {
         res.sendFile(__dirname + '/build/index.html');
     });
 }
