f44cb108f576c60aa2f6cc095c1d99e7a84dedd0
Yan Batista <ybatistasouza@hotmail.com>
2020-09-07 17:34:28 -0300

    add server config

---
+++ server.js
@@ -0,0 +1,17 @@
+import 'dotenv';
+
+import express, { static } from 'express';
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(static(__dirname + '/build'));
+
+if(process.env.NODE_ENV === "production") {
+    app.get('/', (req,res) => {
+        res.sendFile(__dirname + '/build/index.html');
+    });
+}
+
+app.listen(port, () => {
+    console.log('Server is ON');
+})
