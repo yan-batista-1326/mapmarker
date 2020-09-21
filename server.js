de6617d0f1ee517b4a888734af95dae5557c725c
Yan Batista <ybatistasouza@hotmail.com>
2020-09-21 16:58:00 -0300

    added client folder to static express

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/client/build'));
 
 
 if(process.env.NODE_ENV === "production") {
     app.get('*', (req,res) => {
-        res.sendFile(__dirname + 'client/build/index.html');
+        res.sendFile(__dirname + '/client/build/index.html');
     });
 }
 
